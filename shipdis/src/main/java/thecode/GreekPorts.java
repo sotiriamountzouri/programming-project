613e232b65b1f564cf6448f828c7fdf65ce1255f
kapetanellieleni <t8230044@aueb.gr>
2024-11-24 18:30:04 +0200

    Update GreekPorts.java

--- shipdis/src/main/java/thecode/GreekPorts.java
+++ shipdis/src/main/java/thecode/GreekPorts.java
@@ -2,35 +2,6 @@ package thecode;
 import java.util.ArrayList;
 import java.util.List;
 
-class Port {
-    private String island;
-    private String portName;
-    private double latitude;
-    private double longitude;
-
-    public Port(String island, String portName, double latitude, double longitude) {
-        this.island = island;
-        this.portName = portName;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public String getCoordinates() {
-        return portName + " (" + island + "): " + latitude + "° N, " + longitude + "° E";
-    }
-    public double getLatitude() {
-        return latitude;
-    }
-    
-    public double getLongitude() {
-        return longitude;
-    }
-    public String getIsland(){
-        return island;
-    }
-    
-}
-
 public class GreekPorts {
     private List<Port> ports;
 
